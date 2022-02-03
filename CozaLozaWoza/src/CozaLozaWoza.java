d5421bb315a2a4b69b6435d3d5f95c9ea02e3d15
numselli <58607232+numselli@users.noreply.github.com>
2022-02-03 13:20:58 -0800

    make CozaLozaWoza

---
+++ CozaLozaWoza/src/CozaLozaWoza.java
@@ -0,0 +1,21 @@
+
+public class CozaLozaWoza {
+	public static void main(String[] args) { 
+		for (int i = 1; i <= 110; i++) {
+			if (i != 1 && i % 11 == 1) {
+				System.out.print("\n");
+			}
+			if (i % 3 == 0) {
+				System.out.print("Coza ");
+			}else if (i % 5 == 0) {
+				System.out.print("Loza ");
+			}else if (i % 7 == 0){
+				System.out.print("Woza ");
+			} else if(i % 3 == 0 && i % 5 == 0) {
+				System.out.print("CozaLoza ");
+			}else {
+				System.out.print(i+" ");
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # programming12
- 
+- Examples: 
+  - bmi: simple Body Mass Index calculator
+  - CozaLozaWoza: replace divisible numbers with Coza, Loza, and Woza
+- Exercises:
