3add27198d5c934f9ec96da3fa82431ddf6320bb
numselli <58607232+numselli@users.noreply.github.com>
2022-02-03 16:59:03 -0800

    Update CozaLozaWoza.java

--- CozaLozaWoza/src/CozaLozaWoza.java
+++ CozaLozaWoza/src/CozaLozaWoza.java
@@ -1,21 +1,27 @@
-
 public class CozaLozaWoza {
 	public static void main(String[] args) { 
 		for (int i = 1; i <= 110; i++) {
 			if (i != 1 && i % 11 == 1) {
-				System.out.print("\n");
+				System.out.println();
 			}
-			if (i % 3 == 0) {
-				System.out.print("Coza ");
-			}else if (i % 5 == 0) {
-				System.out.print("Loza ");
-			}else if (i % 7 == 0){
-				System.out.print("Woza ");
-			} else if(i % 3 == 0 && i % 5 == 0) {
-				System.out.print("CozaLoza ");
-			}else {
-				System.out.print(i+" ");
-			}
+			
+			System.out.print(check(i));			
 		}
+	}
+	
+	public static String check (int number) {
+		if (number % 3 == 0 && number % 5 == 0) {
+			return "CozaLoza ";
+		} 
+		if (number % 3 == 0) {
+			return "Coza ";
+		}
+		if (number % 5 == 0) {
+			return "Loza ";
+		}
+		if (number % 7 == 0){
+			return "Woza ";		
+		}
+		return Integer.toString(number) + " ";
 	}
 }
