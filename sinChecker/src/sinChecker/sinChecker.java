90d47556a21533c0760cfd878c4caee1e6be41e1
numselli <58607232+numselli@users.noreply.github.com>
2022-02-08 11:36:54 -0800

    Update sinChecker.java

--- sinChecker/src/sinChecker/sinChecker.java
+++ sinChecker/src/sinChecker/sinChecker.java
@@ -9,35 +9,31 @@ public class sinChecker {
 		System.out.println("Input a Social Insurance Number to validate");
 		String inputSIN = (input.nextLine()).replace(" ", "");
 		
-		if (inputSIN.toLowerCase().matches(".*[a-z].*")) {
-			System.out.println("Social Insurance Number can not include letters");
-			return;
-		}
-		if (inputSIN.length() != 9) {
-			System.out.println("Invalid Social Insurance Number length");
+		if (!inputSIN.toLowerCase().matches("[0-9]{9}")) {
+			System.out.println("Social Insurance Number must be a 9 digit long number");
 			return;
 		}
 		
-		System.out.printf("The Social Insurance Number %s is %s",inputSIN, sinCheckerAlgo(inputSIN) ? "valid" : "invalid");
-		
+		String reverseSIN=reverse(inputSIN);
+		System.out.printf("The Social Insurance Number %s is %s",inputSIN, sinCheckerAlgo(reverseSIN) ? "valid" : "invalid");
 	}
 	
 	public static Boolean sinCheckerAlgo(String SIN) {
 		char[] sinChars = SIN.toCharArray();
-		String tempSin = "";
+		int totalDigits = 0;
 		
-		for (int index = 0; index <= sinChars.length-1; index++) {
-			if ((index+1) % 2 == 0) {
-				int doubbledDigit = (Integer.parseInt(String.valueOf(sinChars[index]))*2);
-				tempSin+=String.valueOf(addDigits(doubbledDigit));
-			}else {
-				tempSin+=sinChars[index];
+		for (int index = 0; index <= SIN.length()-1; index++) {
+			String indexChar = String.valueOf(sinChars[index]);
+	
+			int Char = Integer.parseInt(indexChar);
+			
+			if (index !=8) {
+				int multyFactor = ((index+1) % 2 == 0 ? 2 : 1);
+				totalDigits+=addDigits(Char * multyFactor);
 			}
 		}
-		
-		int totalDigits = addDigits(Integer.parseInt(tempSin));
-	
-		return totalDigits % 10 == Integer.parseInt(String.valueOf(sinChars[8]));
+					
+		return 10-(totalDigits % 10) == Integer.parseInt(String.valueOf(sinChars[8]));
 	}
 	
 	public static int addDigits(int inpitDigits) {
@@ -49,5 +45,17 @@ public class sinChecker {
 		}
 		
 		return sum;
+	}
+	
+	public static String reverse(String in) {
+		String nstr = "";
+	    char ch;
+	    
+	    for (int i = 0; i < in.length(); i++){
+	    	ch = in.charAt(i);
+	    	nstr = ch+nstr;
+	    }
+	   
+	   return nstr;
 	}
 }
