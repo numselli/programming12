0c01583cc6294357081131f3d84dbc2cf68a42f9
numselli <58607232+numselli@users.noreply.github.com>
2022-02-16 17:40:05 -0800

    Update craps.java

--- craps/src/craps/craps.java
+++ craps/src/craps/craps.java
@@ -20,35 +20,41 @@ public class craps {
 			int rolledDice = rollDice();
 			
 			if (rollNum == 1) {
+				Integer[] craps = new Integer[] {2, 3, 12};
 				if (rolledDice == 7 || rolledDice == 11) {
 					System.out.println("you won on the first roll");
 					playAgain(input);
-				}
-				
-				Integer[] craps = new Integer[] {2, 3, 12};
-				if (Arrays.asList(craps).contains(rolledDice)) {
+				} else if (Arrays.asList(craps).contains(rolledDice)) {
 					System.out.println("you lose");
 					playAgain(input);
 				}
 				
-			} else if (rollNum != 1 && rolledDice == 7) {
-				System.out.println("you lose");
-				playAgain(input);
-			}
+				Integer[] win = new Integer[] {4, 5, 6, 8, 9, 10};
+				if (Arrays.asList(win).contains(rolledDice)) {
+					point=rolledDice;
+					System.out.println("you win the point\nWant to roll again?\n1) yes\n2) no");
+					int inputChoice = input.nextInt();
+					if (inputChoice == 2) System.exit(0);			
+				}
+			} else {
+				if (rolledDice == 7) {
+					System.out.println("you lose");
+					playAgain(input);					
+				}
 				
-			
-			if (rollNum != 1 && rolledDice == point) {
-				System.out.println("You won. Want to play again?\n1) yes\n2) no");
-				int inputChoice = input.nextInt();
-				if (inputChoice == 1) startGame(input);	
-			}
-			
-			Integer[] win = new Integer[] {4, 5, 6, 8, 9, 10};
-			if (Arrays.asList(win).contains(rolledDice)) {
-				point=rolledDice;
-				System.out.println("you win the point\nWant to roll again?\n1) yes\n2) no");
-				int inputChoice = input.nextInt();
-				if (inputChoice == 2) System.exit(0);			
+				if (rolledDice == point) {
+					System.out.println("You won. Want to play again?\n1) yes\n2) no");
+					int inputChoice = input.nextInt();
+					if (inputChoice == 1) startGame(input);
+				}
+				
+				Integer[] win = new Integer[] {4, 5, 6, 8, 9, 10, 2, 3, 12};
+				if (Arrays.asList(win).contains(rolledDice)) {
+//					point=rolledDice;
+					System.out.println("you win the point\nWant to roll again?\n1) yes\n2) no");
+					int inputChoice = input.nextInt();
+					if (inputChoice == 2) System.exit(0);			
+				}
 			}
 			
 			rollNum++;
