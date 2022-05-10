b264a9035df78f375c8936d85d60b3d898e1bea2
numselli <58607232+numselli@users.noreply.github.com>
2022-05-10 13:23:27 -0700

    update drivers licence

--- driversLicence/src/App.java
+++ driversLicence/src/App.java
@@ -18,11 +18,3 @@ public class App {
         // exam.totalCorrect()
     }
 }
- 
-// A student must correctly answer 8 of the 10 questions to pass the exam.  
-// The class should also have an array field that holds the student’s answers. The class should  
-// have the following methods:  
-
-// ● totalCorrect. Returns the total number of correctly answered questions. Pass the correct answer array and the student answer array to the totalCorrect method 
-// Demonstrate the class in a complete program that asks the user to enter a student’s answers,  
-// and then displays the results returned from the DriverExam class’s methods.  
