e23639f52fb13d2d2c76b24385fb58db1fe23537
numselli <58607232+numselli@users.noreply.github.com>
2022-06-07 15:37:04 -0700

    Update App.java

--- fileMergeSort/src/App.java
+++ fileMergeSort/src/App.java
@@ -1,67 +1,8 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // // make an array with the length of the files
-        // File readFileLengthOne = new File("./fileOne.txt"); 
-        // Scanner readLengthOne = new Scanner(readFileLengthOne);
-        // int fileOneLineLengthCounter = 0;
-        // while(readLengthOne.hasNext()) {  
-        //     readLengthOne.nextLine();
-        //     fileOneLineLengthCounter++;
-        // }
-        // int[] fileOneArray = new int[fileOneLineLengthCounter];
-        // readLengthOne.close(); 
-
-        // File readFileLengthTwo = new File("./fileTwo.txt"); 
-        // Scanner readLengthTwo = new Scanner(readFileLengthTwo);
-        // int fileTwoLineLengthCounter = 0;
-        // while(readLengthTwo.hasNext()) {  
-        //     readLengthTwo.nextLine();
-        //     fileTwoLineLengthCounter++;
-        // }
-        // int[] fileTwoArray = new int[fileTwoLineLengthCounter];
-        // readLengthTwo.close(); 
-
-
-        // // fill the arrays with data from the files
-        // File readFileOne = new File("./fileOne.txt"); 
-        // Scanner readOne = new Scanner(readFileOne);
-        // int fileOneLineCounter = 0;
-        // while(readOne.hasNext()) {  
-        //     int fileOneLine = readOne.nextInt();
-        //     fileOneArray[fileOneLineCounter] = fileOneLine;
-        //     fileOneLineCounter++;
-        // }
-        // readOne.close(); 
-
-        // File readFileTwo = new File("./fileTwo.txt"); 
-        // Scanner readTwo = new Scanner(readFileTwo);
-        // int fileTwoLineCounter = 0;
-        // while(readTwo.hasNext()) {  
-        //     int fileTwoLine = readTwo.nextInt();
-        //     fileTwoArray[fileTwoLineCounter] = fileTwoLine;
-        //     fileTwoLineCounter++;
-        // }
-        // readTwo.close(); 
-
-        // // merge the arrays
-        // int[] outArray = new int[fileOneArray.length + fileTwoArray.length];
-        // System.arraycopy(fileOneArray, 0, outArray, 0, fileOneArray.length);  
-        // System.arraycopy(fileTwoArray, 0, outArray, fileOneArray.length, fileTwoArray.length);  
-
-        // // sort arrays
-        // Arrays.sort(outArray);
- 
-        // // write arrays to file
-        // PrintWriter outputFile = new PrintWriter("./outFile.txt"); 
-        // for (int length=0; length<outArray.length; length++){
-        //     outputFile.println(outArray[length]); 
-        // }
-        // outputFile.close(); 
-
         File readFileOne = new File("./fileOne.txt");
         File readFileTwo = new File("./fileTwo.txt");
         PrintWriter outputFile = new PrintWriter("./outFile.txt"); 
