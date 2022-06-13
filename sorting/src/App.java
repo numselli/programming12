04c93c9a64befaa19bd0440a7728b403a33450d1
numselli <58607232+numselli@users.noreply.github.com>
2022-06-13 10:51:46 -0700

    update sorting

--- sorting/src/App.java
+++ sorting/src/App.java
@@ -12,9 +12,22 @@ public class App {
         }
 
         // sort array
-        Arrays.sort(numersList);
+        sort(numersList);
 
         // print the array
         System.out.println(Arrays.toString(numersList));
+    }
+
+    static void sort(int[] arr) {  
+        int temp = 0;  
+        for(int i = 0; i < arr.length; i++){  
+            for(int j = 1; j < (arr.length-i); j++){  
+                if(arr[j-1] > arr[j]){  
+                    temp = arr[j-1];  
+                    arr[j-1] = arr[j];  
+                    arr[j] = temp;  
+                }              
+            }  
+        }
     }
 }
